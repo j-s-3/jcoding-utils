f38df48990584b94808db1b17faa3a2df84b25e7
jls <joe@redjava.co.uk>
2016-09-21 11:49:22 +0100

    Added util for non null list

--- src/main/java/com/jcodingsolutions/utils/nullutils/NullUtils.java
+++ src/main/java/com/jcodingsolutions/utils/nullutils/NullUtils.java
@@ -1,5 +1,8 @@
 package com.jcodingsolutions.utils.nullutils;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NullUtils {
     public static boolean isNull(Object o) {
         return o == null;
@@ -7,5 +10,9 @@ public class NullUtils {
 
     public static boolean isNotNull(Object o) {
         return !isNull(o);
+    }
+
+    public static <T> List<T> nonNullList(List<T> list) {
+        return list == null ? new ArrayList<>() : list;
     }
 }
--- src/test/java/com/jcodingsolutions/utils/nullutils/NullUtilsTest.java
+++ src/test/java/com/jcodingsolutions/utils/nullutils/NullUtilsTest.java
@@ -2,11 +2,15 @@ package com.jcodingsolutions.utils.nullutils;
 
 import org.junit.Test;
 
-import static com.jcodingsolutions.utils.nullutils.NullUtils.isNotNull;
-import static com.jcodingsolutions.utils.nullutils.NullUtils.isNull;
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.jcodingsolutions.utils.nullutils.NullUtils.*;
 import static org.hamcrest.CoreMatchers.not;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
 
 public class NullUtilsTest {
 
@@ -28,6 +32,19 @@ public class NullUtilsTest {
     @Test
     public void shouldReturnFalseWhenIsNotNullCalledWithNull() {
         assertThat(isNotNull(null), is(not(true)));
+    }
+
+    @Test
+    public void shouldReturnListWhenNotNull() throws Exception {
+        String value = "A";
+        List<String> input = new ArrayList<>();
+        input.add(value);
+        assertThat(nonNullList(input).get(0), is(equalTo(value)));
+    }
+
+    @Test
+    public void shouldReturnEmptyListWhenNull() throws Exception {
+        assertThat(nonNullList(null), is(empty()));
     }
 
 
