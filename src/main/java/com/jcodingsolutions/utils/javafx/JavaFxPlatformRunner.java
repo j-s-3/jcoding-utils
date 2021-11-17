0feb83e1624cc989693fdbc5df7d00927dc24b50
Joseph Stephens <jstephens@sonatype.com>
2021-11-17 13:11:32 -0400

    sca test

--- src/main/java/com/jcodingsolutions/utils/javafx/JavaFxPlatformRunner.java
+++
@@ -1,9 +0,0 @@
-package com.jcodingsolutions.utils.javafx;
-
-import javafx.application.Platform;
-
-public class JavaFxPlatformRunner {
-    public void run(Runnable runnable) {
-        Platform.runLater(runnable);
-    }
-}
